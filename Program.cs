78a71b136a851edb379ea1caea82c071b48ff490
SayedAli <SayedAli@SAYYEDALI>
2025-01-28 09:47:52 +0330

    Edit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using ServerSignalR;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapHub<DataHub>("/dataHub");
 app.Run();
