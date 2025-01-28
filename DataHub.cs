f83c29551a1fa46ae4751ebd4f9abcd405a10e3b
SayedAli <SayedAli@SAYYEDALI>
2025-01-28 09:43:11 +0330

    Add project files.

---
+++ DataHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ServerSignalR
+{
+    public class DataHub : Hub
+    {
+        public async Task SendData(string data)
+        {
+            //await Clients.All.SendAsync("ReceiveData", data);
+            await Clients.Others.SendAsync("ReceiveData", data);
+        }
+    }
+
+}
